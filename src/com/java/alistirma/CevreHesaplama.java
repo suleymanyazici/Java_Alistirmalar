c48314860cfc62c8525ae18a0f80efdf9cc81934
suleymanyazici <suleymanyazc.3@gmail.com>
2025-04-27 14:45:03 +0300

    Daire alanı ve çevresni hesaplayan alıştırma yapıldı

---
+++ src/com/java/alistirma/CevreHesaplama.java
@@ -0,0 +1,24 @@
+package com.java.alistirma;
+
+import java.util.Scanner;
+
+public class CevreHesaplama {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		int r;
+		double cevre, alan;
+		
+		System.out.print("Lütfen dairenin yarıçapının giriniz :");
+		r = input.nextInt();
+		
+		cevre = 2 * Math.PI * r;
+		alan = Math.PI * Math.pow(r, 2);
+		
+		System.out.println("Dairenin alanı   : " + alan);
+		System.out.println("Dairenin çevresi : " + cevre);
+	}
+
+}
